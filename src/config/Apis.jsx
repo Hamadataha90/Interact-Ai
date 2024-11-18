7aa439a718f5abff4b013d1df695aa654868b11b
Hamada Taha <96458235+Hamadataha90@users.noreply.github.com>
2024-11-18 21:40:45 +0200

    Update Apis.jsx

--- src/config/Apis.jsx
+++ src/config/Apis.jsx
@@ -1,10 +1,5 @@
 
 
-// export const apiKey = "AIzaSyCW0g_xJcP8WecPbN8SkR8bcASuTkaaCxI"; 
-// export const apiKey1 = "AIzaSyCJAOC4mj26fkgFcOQKMAWc698Tp75bs0g"; 
-// export const apiKey2 = "AIzaSyA60dkJ_92aze2kN7pXdqlrsQTIq11HZ1M"; 
-// export const apiKey3= "AIzaSyAMxplF1SRnl_38mJabGK5iQuMIyk8-KJo"; 
-
 
 
 export const apiKeys = {
